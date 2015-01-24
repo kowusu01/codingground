3d22cf7d4877a22f47a3c603c54885405eec842c
kowusu01 <kowusu01@gmail.com>
2015-01-23 22:16:35 -0500

    Created by Coding Ground Client

---
+++ ObjCIntro/main.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+
+@interface ICalculator : NSObject
+{
+    int iLeft;
+    int iRight;
+}
+
+- (void) setLeft : (int) nleft; 
+- (void) setLeft : (int) nright;
+
+@end
